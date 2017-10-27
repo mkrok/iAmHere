ca4b33a9268f5ecbdc6710f2e1527c2bf45127f0
mkrok <miroslaw.krok@gmail.com>
2017-10-27 23:34:36 +0200

    webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,11 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
     devtool: 'source-map',
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+             }
+        }),
         new webpack.optimize.UglifyJsPlugin({
             sourceMap: true,
             compress: {
